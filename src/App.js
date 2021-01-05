139724110d8dd06bd5dd48c590955f4ff2987228
BuildTools <unconfigured@null.spigotmc.org>
2021-01-06 00:04:28 +0900

    Study Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,54 +1,38 @@
-function FavoriteIdol({ favname, imgsrc }) {
-  return (
-    <article>
-      <h2>I love {favname}</h2>
+import React from 'react';
 
-      <img src={imgsrc}></img>
-    </article>
-  );
-}
-
-const idolList = [
-  {
-    id: 1,
-    name: 'Futaba Anzu',
-    image:
-      'https://www.project-imas.com/w/images/thumb/b/b1/Anzu_SS.png/450px-Anzu_SS.png',
-  },
-  {
-    id: 2,
-    name: 'Abe Nana',
-    image:
-      'https://www.project-imas.com/w/images/thumb/8/83/Nana_SS.png/450px-Nana_SS.png',
-  },
-  {
-    id: 3,
-    name: 'Shimamura Uzuki',
-    image:
-      'https://www.project-imas.com/w/images/thumb/f/f7/Uzuki_SS.png/450px-Uzuki_SS.png',
-  },
-  {
-    id: 4,
-    name: 'Yumemi Riamu',
-    image:
-      'https://www.project-imas.com/w/images/thumb/d/d5/Riamu_SS.png/450px-Riamu_SS.png',
-  },
-  {
-    id: 5,
-    name: 'Ogata Chieri',
-    image:
-      'https://www.project-imas.com/w/images/thumb/4/46/Chieri_SS.png/450px-Chieri_SS.png',
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {idolList.map((item) => (
-        <FavoriteIdol favname={item.name} imgsrc={item.image} key={item.id} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log('component rendered');
+  }
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+  componentWillUnmount() {
+    console.log('good bye');
+  }
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
