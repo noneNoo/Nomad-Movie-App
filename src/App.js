cee72ae5e70f99ede1c39503277be79f4b2d2563
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 22:52:19 +0900

    Add getMovies function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
     isLodaing: true,
-    movie: [],
+    movies: [],
+  };
+  // axios가 느릴 수 있기 때문에 ansync(비동기)사용
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      'https://yts-proxy.nomadcoders1.now.sh/list_movies.json'
+    );
+    // this.setState({ movies: movies });
+    this.setState({ movies, isLodaing: false });
   };
   // render함수가 실행되고 가장 먼저 실행됨
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLodaing: false });
-    }, 4000);
+    this.getMovies();
   }
   render() {
     const { isLodaing } = this.state;
