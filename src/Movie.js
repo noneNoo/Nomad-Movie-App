b1d1f5085bf92aa00758410b483e159c84685f8f
noneno <noneno@kakao.com>
2021-01-08 16:52:52 +0900

    Styling CSS and Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -17,7 +17,7 @@ function Movie({ year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => {
             return (
               <li key={index} className="genres__genre">
@@ -26,7 +26,7 @@ function Movie({ year, title, summary, poster, genres }) {
             );
           })}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
       </div>
     </div>
   );
